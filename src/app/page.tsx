bc0c24091ad4e7188aa90be3144df65b7bd52b72
Sana86-f83 <sana.faisalmehmood@gmail.com>
2024-11-26 16:21:20 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import Image from "next/image";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
 import Hero from "./components/hero";
-import FeatureWork from "./components/feature-works";
 import Recentpost from "./components/recentpost";
-import TestFeatureWork from "./components/test-feature";
+import FeatureWork from "./components/featurework";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Recentpost />
-      {/* <FeatureWork /> */}
-      <TestFeatureWork />
+      <FeatureWork />
     </div>
   );
 }
